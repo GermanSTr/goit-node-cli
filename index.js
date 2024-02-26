19043bf130050c12eac5697dd1aa468b116f1950
GermanSTr <gershev1994@gmail.com>
2024-02-27 00:45:32 +0200

    full

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { program } from "commander";
+import * as contactsService from "./contacts.js";
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -10,23 +12,29 @@ program.parse();
 
 const options = program.opts();
 
-// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      const allContacts = await contactsService.listContacts();
+      console.log(allContacts);
       break;
-
     case "get":
-      // ... id
+      const ContactById = await contactsService.getContactById(id);
+      console.log(ContactById);
       break;
 
     case "add":
-      // ... name email phone
+      const addNewContact = await contactsService.addContact(
+        name,
+        email,
+        phone
+      );
+      console.log(addNewContact);
       break;
 
     case "remove":
-      // ... id
+      const deleteContactbyID = await contactsService.removeContact(id);
+      console.log(deleteContactbyID);
       break;
 
     default:
